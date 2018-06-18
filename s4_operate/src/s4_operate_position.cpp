d64955433a9874d584c76eecbde49bc429402a0d
erio <erio@ubuntu>
2018-06-18 22:59:14 +0900

    each rviz: modify s4_launch: modify

--- s4_operate/src/s4_operate_position.cpp
+++ s4_operate/src/s4_operate_position.cpp
@@ -14,20 +14,24 @@ void odm_callback(const geometry_msgs::Twist& odm_msg){
     odm_now=odm_msg;
 }
 
-void set_robot(float *position, float *direction){
+void set_robot(float *position, float *direction, std::string base_link, std::string world_link){
 	static tf::TransformBroadcaster br;
 	tf::Transform transform;
 	transform.setOrigin( tf::Vector3(position[0], position[1], position[2]) );
 	tf::Quaternion q;
 	q.setRPY(direction[0], direction[1], direction[2]);
 	transform.setRotation(q);
-	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "robot0/base_link"));	
+	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), world_link, base_link));	
 }
 
 int main(int argc, char **argv){
 	ros::init(argc, argv, "s4_operate_position");
 	ros::NodeHandle n;
 	ros::NodeHandle pn("~");
+	std::string world_link="";
+	std::string base_link="";
+	pn.getParam("world_link", world_link);
+	pn.getParam("base_link",  base_link);
 		
     //subscriibe
 	ros::Subscriber odm_sub   = n.subscribe("odm_vel", 10, odm_callback);
@@ -44,7 +48,7 @@ int main(int argc, char **argv){
 		direction[0]=0.0;
 		direction[1]=0.0;
 		direction[2]+=odm_now.angular.z*dt;
-		set_robot(position,direction);
+		set_robot(position, direction, base_link, world_link);
         ros::spinOnce();
 		loop_rate.sleep();
 	}
