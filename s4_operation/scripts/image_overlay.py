56a4463b0a2aab8f76c23ed1e0f18149f86cf900
erio <erio@ubuntu>
2019-05-03 22:23:28 +0900

    s4_operation: modify

--- s4_operation/scripts/image_overlay.py
+++ s4_operation/scripts/image_overlay.py
@@ -5,7 +5,7 @@ import cv2
 from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
-from s4_msgs.msg import TrackedObjectArray
+from s4_msgs.msg import TrackedObjectArray, TrackedInfo
 
 class hsvFilter:
     def __init__(self):
@@ -17,6 +17,8 @@ class hsvFilter:
         self.image_sub = rospy.Subscriber("input_image", Image, self.image_callback, queue_size=1)
         self.objects_sub = rospy.Subscriber("input_objects", TrackedObjectArray, self.objects_callback, queue_size=1)
         self.last_objects = TrackedObjectArray()
+        self.focus_sub = rospy.Subscriber("focus", TrackedInfo, self.focus_callback, queue_size=1)
+        self.last_focus = TrackedInfo()
 
     def image_callback(self, ros_image):
         try:
@@ -27,8 +29,11 @@ class hsvFilter:
 
         #objects
         for object in self.last_objects.objects:
+            color = (0, 255, 0)
+            if self.last_focus.category == object.info.category and self.last_focus.id == object.info.id:
+                color = (255, 100, 100)
             r = object.rect
-            cv2.rectangle(image, (r.x, r.y), (r.x + r.width, r.y + r.height), (0, 255, 0), thickness=3)
+            cv2.rectangle(image, (r.x, r.y), (r.x + r.width, r.y + r.height), color, thickness=3)
             text1 = object.info.category
             text2 = "id:" + str(object.info.id)
             cv2.putText(image, text1, (r.x, r.y - 25), cv2.FONT_HERSHEY_PLAIN, 1.5,(0, 255, 0), thickness=2)
@@ -40,6 +45,9 @@ class hsvFilter:
 
     def objects_callback(self, objects_msg):
         self.last_objects = objects_msg
+
+    def focus_callback(self, focus_msg):
+        self.last_focus = focus_msg
 
     def cleanup(self):
         cv2.destroyAllWindows()   
--- s4_operation/src/s4_operation_test_app.cpp
+++ s4_operation/src/s4_operation_test_app.cpp
@@ -4,6 +4,7 @@
 #include <actionlib/client/simple_action_client.h>
 #include <s4_msgs/GameAppAction.h>
 #include <s4_msgs/TrackedObjectArray.h>
+#include <algorithm>
 
 #define PS3_Button_Max 17
 #define PS3_Select 0
@@ -23,10 +24,13 @@
 #define PS3_cross    14
 #define PS3_square   15
 #define PS3_PS 16
+
 #define PS3_LH 0
 #define PS3_LV 1
 #define PS3_RH 2
 #define PS3_RV 3
+#define PS3_AL2 12
+#define PS3_AR2 13
 
 typedef actionlib::SimpleActionClient<s4_msgs::GameAppAction> Client;
 
@@ -34,10 +38,19 @@ float scale_linear_x=0.3;
 float scale_linear_y=0.3;
 float scale_angular_z=2.5;
 
-ros::Time last_joy_stamp;
+std::vector<int> buttons_press;
+std::vector<int> buttons_release;
 sensor_msgs::Joy last_joy_msg;
 void joy_callback(const sensor_msgs::Joy& joy_msg){
-  last_joy_stamp = joy_msg.header.stamp;
+  int num = std::min(joy_msg.buttons.size(), last_joy_msg.buttons.size());
+  buttons_press.resize(num);
+  buttons_release.resize(num);
+  for (int i=0; i< num; i++){
+    if(joy_msg.buttons[i] && !last_joy_msg.buttons[i]) buttons_press[i] = 1;
+    else buttons_press[i] = 0;
+    if(!joy_msg.buttons[i] && last_joy_msg.buttons[i]) buttons_release[i] = 1;
+    else buttons_release[i] = 0;
+  }
   last_joy_msg = joy_msg;
 }
 
@@ -63,6 +76,7 @@ int main(int argc, char** argv){
   ROS_INFO("START");
   Client move_client("move_action", false);
   Client turret_client("turret_action", false);
+  Client select_client("select_action", false);
 
   ros::Subscriber joy_sub = nh.subscribe("joy", 1, joy_callback);
   ros::Subscriber object_sub = nh.subscribe("objects", 10, object_callback);
@@ -70,69 +84,108 @@ int main(int argc, char** argv){
   ros::Rate loop_rate(10);
   
   while (ros::ok()){
-    if(ros::Time::now() - last_joy_stamp < ros::Duration(1.0)){
-      //move
-      if(move_client.isServerConnected()){
-        ROS_INFO_ONCE("move server connected");
-        geometry_msgs::Twist cmd_vel;
-        cmd_vel.linear.x =last_joy_msg.axes[PS3_LV]*scale_linear_x;
-        cmd_vel.linear.y =last_joy_msg.axes[PS3_LH]*scale_linear_y;
-        cmd_vel.angular.z=last_joy_msg.axes[PS3_RH]*scale_angular_z;
+    if(ros::Time::now() - last_joy_msg.header.stamp < ros::Duration(1.0)){
+      if(std::min(std::min(last_joy_msg.buttons.size(), buttons_press.size()), buttons_release.size())>=PS3_Button_Max){
+        //move
+        if(move_client.isServerConnected()){
+          ROS_INFO_ONCE("move server connected");
+          geometry_msgs::Twist cmd_vel;
+          float move_scale = 0.5;
+          if(last_joy_msg.buttons[PS3_L1])move_scale=1.0;
 
-        s4_msgs::GameAppGoal goal1;
-        goal1.header.stamp=ros::Time::now();
-        goal1.mode=s4_msgs::GameAppGoal::NAV_VEL;
-        goal1.twist=cmd_vel;
-        goal1.duration=3.0;
-        move_client.sendGoal(goal1);
-      }
-      //turret
-      if(turret_client.isServerConnected()){//laser on
-        ROS_INFO_ONCE("turret server connected");
-        s4_msgs::GameAppGoal goal2;
-        if(last_joy_msg.buttons[PS3_triangle]){
-          goal2.header.stamp=ros::Time::now();
-          goal2.mode=s4_msgs::GameAppGoal::FC_LASER;
-          goal2.on=true;
-          turret_client.sendGoal(goal2);
+          cmd_vel.linear.x = move_scale * last_joy_msg.axes[PS3_LV] * scale_linear_x;
+          cmd_vel.angular.z = move_scale * last_joy_msg.axes[PS3_LH] * scale_angular_z;
+          
+          if(last_joy_msg.axes[PS3_AL2]==1 && last_joy_msg.axes[PS3_AR2]<1){
+            cmd_vel.linear.y = -move_scale * scale_linear_y * std::min(1.0-last_joy_msg.axes[PS3_AR2], 1.0);
+          }
+          else if(last_joy_msg.axes[PS3_AR2]==1 && last_joy_msg.axes[PS3_AL2]<1){
+            cmd_vel.linear.y = move_scale * scale_linear_y * std::min(1.0-last_joy_msg.axes[PS3_AL2], 1.0);
+          }
+          else{
+            cmd_vel.linear.y = 0.0;
+          }
+          s4_msgs::GameAppGoal goal1;
+          goal1.header.stamp=ros::Time::now();
+          goal1.mode=s4_msgs::GameAppGoal::NAV_VEL;
+          goal1.twist=cmd_vel;
+          goal1.duration=3.0;
+          move_client.sendGoal(goal1);
         }
-        else if(last_joy_msg.buttons[PS3_square]){//laser off
-          goal2.header.stamp=ros::Time::now();
-          goal2.mode=s4_msgs::GameAppGoal::FC_LASER;
-          goal2.on=false;
-          turret_client.sendGoal(goal2);
-        }
-        else if(last_joy_msg.buttons[PS3_R2] && last_joy_msg.buttons[PS3_R1]){//shot
-          goal2.header.stamp=ros::Time::now();
-          goal2.mode=s4_msgs::GameAppGoal::FC_SHOT;
-          goal2.on=true;
-          turret_client.sendGoal(goal2);
-        }
+        //turret
+        if(turret_client.isServerConnected()){
+          ROS_INFO_ONCE("turret server connected");
+          s4_msgs::GameAppGoal goal2;
+          if(buttons_press[PS3_R1]){//laser on
+            goal2.header.stamp=ros::Time::now();
+            goal2.mode=s4_msgs::GameAppGoal::FC_LASER;
+            goal2.on=true;
+            turret_client.sendGoal(goal2);
+          }
+          else if(buttons_press[PS3_triangle]){//laser off
+            goal2.header.stamp=ros::Time::now();
+            goal2.mode=s4_msgs::GameAppGoal::FC_LASER;
+            goal2.on=false;
+            turret_client.sendGoal(goal2);
+          }
+          else if(last_joy_msg.buttons[PS3_R1] && buttons_press[PS3_cross]){//shot
+            goal2.header.stamp=ros::Time::now();
+            goal2.mode=s4_msgs::GameAppGoal::FC_SHOT;
+            goal2.on=true;
+            turret_client.sendGoal(goal2);
+          }
 
-        static geometry_msgs::Point aim_pos;
-        bool isAimMove=last_joy_msg.buttons[PS3_Right]||last_joy_msg.buttons[PS3_Left]||last_joy_msg.buttons[PS3_Up]||last_joy_msg.buttons[PS3_Down];
-        if(isAimMove){
-          float dt=0.1;
-          aim_pos.x=1.0;
-          if(last_joy_msg.buttons[PS3_Right])aim_pos.y += -0.4 * dt;
-          if(last_joy_msg.buttons[PS3_Left])aim_pos.y += 0.4 * dt;
-          if(last_joy_msg.buttons[PS3_Up])aim_pos.z += -0.05 * dt;
-          if(last_joy_msg.buttons[PS3_Down])aim_pos.z += 0.05 * dt;
-          goal2.header.stamp=ros::Time::now();
-          goal2.mode=s4_msgs::GameAppGoal::FC_POS;
-          goal2.pose.position=aim_pos;
-          goal2.duration=3.0;
-          turret_client.sendGoal(goal2);
+          static geometry_msgs::Point aim_pos;
+          float aim_scale = 1.0;
+          if(buttons_press[PS3_square]){//aim center
+            aim_pos.y = 0.0;
+            aim_pos.z = 0.0;
+            goal2.header.stamp=ros::Time::now();
+            goal2.mode=s4_msgs::GameAppGoal::FC_POS;
+            goal2.pose.position=aim_pos;
+            goal2.duration=3.0;
+            turret_client.sendGoal(goal2);
+          }
+          else if(std::fabs(last_joy_msg.axes[PS3_RH]) + std::fabs(last_joy_msg.axes[PS3_RV]) > 0.01){
+            float aim_scale = 1.0;
+            if(last_joy_msg.buttons[PS3_R1])aim_scale = 0.3;
+            static geometry_msgs::Twist aim_vel;
+            aim_vel.linear.y = aim_scale * 0.8 * last_joy_msg.axes[PS3_RH];
+            aim_vel.linear.z = aim_scale * 0.2 * -last_joy_msg.axes[PS3_RV];
+
+            goal2.header.stamp=ros::Time::now();
+            goal2.mode=s4_msgs::GameAppGoal::FC_VEL;
+            goal2.twist = aim_vel;
+            goal2.duration=0.1;
+            turret_client.sendGoal(goal2);
+          }
+          else if(buttons_press[PS3_circle]){
+            goal2.header.stamp=ros::Time::now();
+            //goal2.mode=s4_msgs::GameAppGoal::FC_OBJECT;
+            goal2.mode=s4_msgs::GameAppGoal::FC_FOCUS;
+            goal2.duration=3.0;
+            goal2.info.category="red_can";
+            goal2.info.id=gun_tracked_id;
+            turret_client.sendGoal(goal2);
+          }
         }
-        else if(last_joy_msg.buttons[PS3_circle]){
-          goal2.header.stamp=ros::Time::now();
-          goal2.mode=s4_msgs::GameAppGoal::FC_OBJECT;
-          goal2.duration=3.0;
-          goal2.info.category="red_can";
-          goal2.info.id=gun_tracked_id;
-          turret_client.sendGoal(goal2);
+        if(select_client.isServerConnected()){
+          s4_msgs::GameAppGoal goal2;
+          if(buttons_press[PS3_Up]){//focus
+            goal2.header.stamp=ros::Time::now();
+            goal2.mode=s4_msgs::GameAppGoal::SL_CHANGE;
+            goal2.duration=1.0;
+            select_client.sendGoal(goal2);
+          }
         }
       }
+    }
+    //buttons reset
+    for (int i=0; i< (int)buttons_press.size(); i++){
+      buttons_press[i] = 0;
+    }
+    for (int i=0; i< (int)buttons_release.size(); i++){
+      buttons_release[i] = 0;
     }
     ros::spinOnce();
     loop_rate.sleep();
